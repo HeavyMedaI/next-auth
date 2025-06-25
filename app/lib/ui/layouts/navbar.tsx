8d1ef1aa14a5f44f24105ea87dc5e63ba627911e
Musa ATALAY <musaatalay.developer@gmail.com>
2025-06-26 01:16:31 +0300

    There was a type issue with an unknown string variable, I can not see them in dev runtime

--- app/lib/ui/layouts/navbar.tsx
+++ app/lib/ui/layouts/navbar.tsx
@@ -31,9 +31,9 @@ export default function Navbar() {
     const {data: session} = useSession()
 
     const user = {
-        name: session?.user?.name,
-        email: session?.user?.email,
-        imageUrl: session?.user?.image,
+        name: session?.user?.name as string,
+        email: session?.user?.email as string,
+        imageUrl: session?.user?.image as string,
     }
 
     return <Disclosure as="nav" className="bg-gray-800">
@@ -82,7 +82,7 @@ export default function Navbar() {
                                 <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
                                     <span className="absolute -inset-1.5" />
                                     <span className="sr-only">Open user menu</span>
-                                    <img alt={user?.name as string} src={user.imageUrl as string} className="size-8 rounded-full" />
+                                    <img alt={user.name} src={user.imageUrl} className="size-8 rounded-full" />
                                 </MenuButton>
                             </div>
                             <MenuItems
@@ -136,7 +136,7 @@ export default function Navbar() {
             <div className="border-t border-gray-700 pt-4 pb-3">
                 <div className="flex items-center px-5">
                     <div className="shrink-0">
-                        <img alt={user?.name as string} src={user?.imageUrl as string} className="size-10 rounded-full" />
+                        <img alt={user.name} src={user.imageUrl} className="size-10 rounded-full" />
                     </div>
                     <div className="ml-3">
                         <div className="text-base/5 font-medium text-white">{user.name}</div>
